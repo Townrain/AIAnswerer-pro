21e2fb7ec89c131d11d553e52d80eda50924779b
AIAnswerer <dev@aianswerer.local>
2026-07-18 19:28:13 +0800

    test: 修复 Dispatchers.setMain/resetMain 导入

--- app/src/test/java/com/hwb/aianswerer/FloatingWindowViewModelTest.kt
+++ app/src/test/java/com/hwb/aianswerer/FloatingWindowViewModelTest.kt
@@ -7,7 +7,9 @@ import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
